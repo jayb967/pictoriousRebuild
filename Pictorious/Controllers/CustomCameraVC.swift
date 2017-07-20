47e5a8c01ed430f5b29319f734f4431073e54d1e
Serg T <sergtsaeb@gmail.com>
2017-07-19 17:00:43 -0700

    cameraVC setup, import AVFoundation

--- Pictorious/Controllers/CustomCameraVC.swift
+++ Pictorious/Controllers/CustomCameraVC.swift
@@ -7,36 +7,33 @@
 //
 
 import UIKit
+import Foundation
+import AVFoundation
 
 class CustomCameraVC: UIViewController {
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var cameraView: UIView!
+    
+    let captureSession = AVCaptureSession()
+    let photoOutput = AVCapturePhotoOutput()
+    var previewLayer : AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        
+        
     }
     
     @IBAction func didPressCameraButton(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
-        let cameraVC = storyboard.instantiateViewController(withIdentifier: "createVC")
-        present(cameraVC, animated: true, completion: nil)
+//        let cameraVC = storyboard.instantiateViewController(withIdentifier: "createVC")
+//        present(cameraVC, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
