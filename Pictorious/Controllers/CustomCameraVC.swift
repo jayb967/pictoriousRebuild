4853553c9256308f741558800edee417c3ef461b
Serg T <sergtsaeb@gmail.com>
2017-07-21 12:18:31 -0700

    AVCapturePhotoOutput implementation with delegate observer

--- Pictorious/Controllers/CustomCameraVC.swift
+++ Pictorious/Controllers/CustomCameraVC.swift
@@ -10,98 +10,128 @@ import AVFoundation
 
 class CustomCameraVC: UIViewController {
     
-    var captureSession = AVCaptureSession()
-    let stillImageOutput = AVCapturePhotoOutput()
+    var session: AVCaptureSession?
     var previewLayer : AVCaptureVideoPreviewLayer?
+    var stillImageOutput = AVCapturePhotoOutput()
+    var cameraPosition = "back"
     
     // storage for found device at initialization
     var captureDevice : AVCaptureDevice?
     
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var imgOverlay: UIImageView!
+    @IBOutlet weak var cameraView: UIView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Setup your camera here...
+        previewLayer!.frame = cameraView.bounds
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        loadCamera()
+    }
+    
+    func loadCamera() {
+        session?.stopRunning()
+        previewLayer?.removeFromSuperlayer()
         
-        if let devices = AVCaptureDevice.devices() as? [AVCaptureDevice] {
-            
-            // Loop through all the capture devices on this phone
-            for device in devices {
-                // Make sure this particular device supports video
-                if (device.hasMediaType(AVMediaTypeVideo)) {
-                    // Finally check the position and confirm we've got the back camera
-                    if(device.position == AVCaptureDevicePosition.front) {
-                        captureDevice = device
-                        if captureDevice != nil {
-                            print("Capture device found")
-                            beginSession()
-                        }
-                    }
-                }
-            }
+        session = AVCaptureSession()
+        session!.sessionPreset = AVCaptureSessionPresetHigh
+        
+        var backCamera = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .front)
+        
+        if cameraPosition == "back"
+        {
+            backCamera = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back)
         }
-
+        
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+            print(error!.localizedDescription)
+        }
+        
+        if error == nil && session!.canAddInput(input) {
+            session!.addInput(input)
+            
+            stillImageOutput = AVCapturePhotoOutput()
+            
+            if session!.canAddOutput(stillImageOutput) {
+                session!.addOutput(stillImageOutput)
+                previewLayer = AVCaptureVideoPreviewLayer(session: session)
+                previewLayer?.frame = cameraView.bounds
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                session!.startRunning()
+                
+            }
+            
+        }
+            
     }
     
     
     func beginSession() {
         
-        do {
-            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-//            stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
-            
-            if captureSession.canAddOutput(stillImageOutput) {
-                captureSession.addOutput(stillImageOutput)
-            }
-            
-        }
-        catch {
-            print("error: \(error.localizedDescription)")
-        }
+        //configure the output
+        let delegate = AVCapturePhotoCaptureDelegate()
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                             kCVPixelBufferWidthKey as String: 160,
+                             kCVPixelBufferHeightKey as String: 160,
+                             ]
+        settings.previewPhotoFormat = previewFormat
         
-        guard let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) else {
+        stillImageOutput.capturePhoto(with: settings, delegate: delegate)
+        
+
+        guard let previewLayer = AVCaptureVideoPreviewLayer(session: session) else {
             print("no preview layer")
             return
         }
         
         self.view.layer.addSublayer(previewLayer)
         previewLayer.frame = self.view.layer.frame
-        captureSession.startRunning()
+        session!.startRunning()
+        
         
         self.view.addSubview(imgOverlay)
         self.view.addSubview(cameraButton)
     }
     
-  
-        
-
     func saveToCamera() {
         
-        if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo) {
-            
-            stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (CMSampleBuffer, Error) in
-                if let imageData = AVCapturePhotoOutput.jpegStillImageNSDataRepresentation(CMSampleBuffer) {
-                    
-                    if let cameraImage = UIImage(data: imageData) {
-                        
-                        UIImageWriteToSavedPhotosAlbum(cameraImage, nil, nil, nil)
-                    }
-                }
-            })
-        }
+//        if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo) {
+//
+//            stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (CMSampleBuffer, Error) in
+//                if let imageData = AVCapturePhotoOutput.jpegStillImageNSDataRepresentation(CMSampleBuffer) {
+//
+//                    if let cameraImage = UIImage(data: imageData) {
+//
+//                        UIImageWriteToSavedPhotosAlbum(cameraImage, nil, nil, nil)
+//                    }
+//                }
+//            })
+//        }
     }
   
-    
 
-    
     @IBAction func didPressCameraButton(_ sender: UIButton) {
-        
         print("Camera button pressed")
         saveToCamera()
         
+    
 //        let storyboard = UIStoryboard(name: "Main", bundle: nil)
 //        let cameraVC = storyboard.instantiateViewController(withIdentifier: "createVC")
 //        present(cameraVC, animated: true, completion: nil)
